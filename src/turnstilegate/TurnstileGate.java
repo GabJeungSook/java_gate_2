15a825e13686563fa7aed33167394e0bf2ddcc1b
GabJeungSook <gicawalo@gmail.com>
2024-05-07 15:21:51 +0800

    timeout

--- src/turnstilegate/TurnstileGate.java
+++ src/turnstilegate/TurnstileGate.java
@@ -95,7 +95,7 @@ public class TurnstileGate implements INConnectorEvent  {
           
     public CommandDetail getCommandDetail() { 
         TCPClientDetail tcpClient = new TCPClientDetail("192.168.1.151", 8000);
-        tcpClient.Timeout = 5000;//连接超时时间（毫秒）
+        tcpClient.Timeout = 10000;//连接超时时间（毫秒）
         tcpClient.RestartCount = 0;//重新连接次数		
         Door8800Identity idt = new Door8800Identity("MC-5924T23010061", "FFFFFFFF", E_ControllerType.Door8900);
         CommandDetail commandDetail = new CommandDetail();
